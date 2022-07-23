16bd4d5e5d9a73cf6db10dfd24dc841e58641219
Robert Coffey <rjc@robertcoffey.xyz>
2022-07-22 21:38:30 -0230

    set type of sexp to SEXP_CLOSURE in make_closure

--- src/closure.c
+++ src/closure.c
@@ -8,17 +8,17 @@
 #include "types.h"
 
 sexp_t *make_closure(sexp_t *args, sexp_t *body, sexp_t *env) {
-    assert(args); assert(sexp_is_cons(args));
-    assert(body); assert(sexp_is_cons(body));
-    assert(env); assert(sexp_is_cons(env));
-    return sexp_cons();
+    assert(args); assert(body); assert(env);
+    assert(sexp_is_cons(args)); assert(sexp_is_cons(env));
+    sexp_t *clos = sexp_cons(args, sexp_cons(body, sexp_cons(env, sexp_null)));
+    sexp_type_set(clos, SEXP_CLOSURE);
+    return clos;
 }
 
 static sexp_t *ref(const sexp_t *clos,
                    sexp_t *(*func)(const sexp_t *s))
 {
-    assert(clos);
-    assert(func);
+    assert(clos); assert(func);
     return (sexp_type(clos) == SEXP_CLOSURE) ? func(clos) : NULL;
 }
 
